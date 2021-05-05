79fcdb24304cd0fcfac3cd8eda6d39a024e1cf46
jen615 <xjenghisx@gmail.com>
2021-05-04 23:25:15 -0700

    update book class to contain default download folder (startDirectory)

--- src/classes/book_class.py
+++ src/classes/book_class.py
@@ -8,6 +8,7 @@ class Book:
     """Instructions on type of book to build & chapters to download"""
 
     def __init__(self, manga: Manga, bookType: str):
+        self.startDir = f'{Path.home()}/Downloads/Baka'
         self.manga = manga
         self.type = bookType
 
@@ -15,21 +16,20 @@ class Book:
     finish: int = None
     directory: str
 
-    def create_dirs(self):
+    def create_dirs(self) -> None:
         manga = self.manga
-        startDir = f'{Path.home()}/Downloads/Baka'
         folderName = f'{manga.title}'
 
         if self.type == 'chapter':
             self.start = int(input(f'Which chapter of {manga.title} do you want to download?'))
             self.directory = f'{manga.title} Chapter {self.start}'
-            os.makedirs(f'{startDir}/{folderName}', exist_ok=True)
-            os.makedirs(f'{startDir}/{self.directory}', exist_ok=True)
+            os.makedirs(f'{self.startDir}/{folderName}', exist_ok=True)
+            os.makedirs(f'{self.startDir}/{self.directory}', exist_ok=True)
             return
 
         elif self.type == 'collection':
             self.start = int(input('From which chapter of One Piece do you want to start? '))
             self.finish = int(input(' At which chapter do you want to end? '))
             self.directory = f'{manga.title} {self.start}-{self.finish}'
-            os.makedirs(f'{startDir}/{folderName}', exist_ok=True)
+            os.makedirs(f'{self.startDir}/{folderName}', exist_ok=True)
             return
