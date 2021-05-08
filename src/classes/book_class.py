713467776dda468ddb51e1f55bbbd0016b8b59ea
jen615 <xjenghisx@gmail.com>
2021-05-08 02:39:46 -0700

    dir maintenance: - move chapter dir logic to chapter download function - add cleanup function - add series var

--- src/classes/book_class.py
+++ src/classes/book_class.py
@@ -11,25 +11,35 @@ class Book:
         self.startDir = f'{Path.home()}/Downloads/Baka'
         self.manga = manga
         self.type = bookType
+        self.series = f'{self.startDir}/{manga.title}'
 
     start: int = None
     finish: int = None
-    directory: str
+    series: str
+    chapDir: str = None
+    bookDir: str = None
 
-    def create_dirs(self) -> None:
+    def chapter_info(self) -> None:
         manga = self.manga
-        folderName = f'{manga.title}'
 
         if self.type == 'chapter':
             self.start = int(input(f'Which chapter of {manga.title} do you want to download?'))
-            self.directory = f'{manga.title} Chapter {self.start}'
-            os.makedirs(f'{self.startDir}/{folderName}', exist_ok=True)
-            os.makedirs(f'{self.startDir}/{self.directory}', exist_ok=True)
+            self.chapDir = f'{self.series}/{manga.title} Chapter {self.start}'
+            os.makedirs(f'{self.series}', exist_ok=True)  # Series folder
             return
 
         elif self.type == 'collection':
             self.start = int(input('From which chapter of One Piece do you want to start? '))
             self.finish = int(input(' At which chapter do you want to end? '))
-            self.directory = f'{manga.title} {self.start}-{self.finish}'
-            os.makedirs(f'{self.startDir}/{folderName}', exist_ok=True)
+            self.bookDir = f'{manga.title} {self.start}-{self.finish}'
+            os.makedirs(f'{self.series}', exist_ok=True)  # Series folder
             return
+
+    def cleanup(self, collection: bool = False) -> None:
+        """Cleans up files and folders upon success or failure.
+            Accepts a collection boolean to know what files to remove"""
+        if not collection:
+            # os.remove(f'{self.chapDir}.cbz')
+            for file in os.listdir(f'{self.series}/{self.start}'):
+                os.remove(f'{self.series}/{self.start}/{file}')
+            os.rmdir(f'{self.series}/{self.start}')
