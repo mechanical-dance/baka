a7659b63b3af3b6a11354ddbe308b435bd8f6706
jen615 <xjenghisx@gmail.com>
2021-05-08 02:41:47 -0700

    add data check and chapter prep functions reformat dir init

--- src/scraper_functions.py
+++ src/scraper_functions.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 import bs4
 import requests
@@ -6,9 +7,16 @@ import requests
 from src.classes.book_class import Book
 
 
-def download_chapter(book: Book):
+def download_chapter(book: Book, chapter: int = None):
+    """Finds a manga chapter, loops through and downloads its images.
+    Accepts an optional 'chapter' value for looping through chapters in collection mode."""
+
+    chapter = chapter if chapter else book.start
+
+    os.makedirs(f'{book.series}/{chapter}', exist_ok=True)
+
     try:
-        res = requests.get(f'{book.manga.url}{book.start}')
+        res = requests.get(f'{book.manga.url}{chapter}')
     except ConnectionError:
         print('It seems the info for that manga is outdated. Please open an issue')  # Todo: Link to github
         # Todo Cleanup Directories
@@ -18,28 +26,44 @@ def download_chapter(book: Book):
     imageArray: [str] = parser.select(book.manga.div_structure)
 
     if len(imageArray) == 0:
-        print(f"Couldn't find chapter {book.start}. Exiting...")
+        print(f"Couldn't find chapter {chapter}. Exiting...")
         exit(0)
 
     count: int = 0
 
-    for image in imageArray:
-        url = imageArray[count].get('src')
-        # Check to see if the source is normal or 'data-source' type
-        if 'data' in url:
-            # Nested/ broken loop so that source type only needs to be checked once
-            for dataImage in imageArray:
-                url = imageArray[count].get('data-src')
-                download_img(book, url, count)
-            break
-        download_img(book, url, count)
+    data = data_check(imageArray[0])
+
+    if data:
+        for image in imageArray:
+            url = imageArray[count].get('data-src')
+            download_img(book, chapter, url, count)
+            count += 1
+    else:
+        for image in imageArray:
+            url = imageArray[count].get('src')
+            download_img(book, chapter, url, count)
+            count += 1
+
+    prepare_cbz(book, chapter)
 
 
-def download_img(book: Book, url: str, count: int):
+def prepare_cbz(book: Book, chapter: int) -> None:
+    arch = shutil.make_archive(f'{book.series}/{chapter}', 'zip', f'{book.series}/{chapter}')  # Archive chapter folder
+    cbz = f'{book.chapDir}.cbz' if book.chapDir else f'{book.series}/{chapter}.cbz'
+    os.rename(arch, cbz)
+
+
+def data_check(img) -> bool:
+    """Check to see if source is normal or data-type"""
+    url = img.get('src')
+    return True if 'data' in url else False
+
+
+def download_img(book: Book, chapter, url: str, count: int):
     print(f'Downloading {url}')
     res2 = requests.get(url)
     res2.raise_for_status()
-    pic = open(os.path.join(f'{book.startDir}/{book.directory}', os.path.basename(url)), 'wb')
+    pic = open(os.path.join(f'{book.series}/{chapter}', os.path.basename(url)), 'wb')
     for i in res2.iter_content(100000):
         pic.write(i)
     pic.close()
