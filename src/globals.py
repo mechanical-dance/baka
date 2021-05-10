25c96aed5c7027ed6cb6352891e5cbe4553b4649
jen615 <xjenghisx@gmail.com>
2021-05-09 19:01:54 -0700

    update calibre2 file location for 64bit windows

--- src/globals.py
+++ src/globals.py
@@ -2,4 +2,4 @@ import platform
 from pathlib import Path
 
 CALIBRE_LOCATION = '/Applications/calibre.app/Contents/MacOS/ebook-convert' if platform.system() == 'Darwin' \
-    else Path("C:/Program Files (x86)/calibre/ebook-convert")
+    else Path("C:/Program Files/calibre2/ebook-convert")
